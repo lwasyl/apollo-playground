aa296d86b4d510af3b88c80d0e9f0a0eba83fc71
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2023-05-08 23:23:36 +0200

    Fix repro

--- src/test/kotlin/com/example/Test.kt
+++ src/test/kotlin/com/example/Test.kt
@@ -1,6 +1,6 @@
 package com.example
 
-import com.apollo.repro.BooksByIDQuery
+import com.apollo.repro.BooksByAuthorQuery
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.first
@@ -13,10 +13,10 @@ class Test : TestBase() {
     @Test
     fun `cache only`() = runBlocking {
         mockWebServer.enqueue(response)
-        val (cache_only1, job1) = watch(query = BooksByIDQuery(bookId = "fixture-id"))
+        val (cache_only1, job1) = watch(query = BooksByAuthorQuery(authorId = "fixture-id"))
         cache_only1.readValue().data shouldBe null // empty cache = empty response
 
-        val refreshed = refresh(query = BooksByIDQuery(bookId = "fixture-id"))
+        val refreshed = refresh(query = BooksByAuthorQuery(authorId = "fixture-id"))
         refreshed.data shouldBe cache_only1.readValue().data
 
         cache_only1.cancel()
